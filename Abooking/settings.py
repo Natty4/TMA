8142afb17c28bf0bfbdbf9f44d99f86ad3fc46e6
Sonic4 <natan2kt@gmail.com>
2024-08-13 01:29:49 +0300

    Update database configuration and dependencies

--- Abooking/settings.py
+++ Abooking/settings.py
@@ -79,11 +79,20 @@ WSGI_APPLICATION = 'Abooking.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 
