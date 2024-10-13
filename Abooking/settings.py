2d4477814f7c68727584f08b34211fa67c35973e
natty4 <natty7kt@gmail.com>
2024-10-14 01:57:16 +0300

    Update API base URL to use environment variable

--- Abooking/settings.py
+++ Abooking/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 if DEBUG:
     API_BASE_URL = 'http://127.0.0.1:8000/api/'
 else:
-    API_BASE_URL = 'https://tminiapp-1a4r6fw7y-scotts-projects-1f569b96.vercel.app/api/'
+    API_BASE_URL = os.getenv('API_BASE_URL')
     
 
 # Application definition
