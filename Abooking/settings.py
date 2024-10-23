1bfa74001a0a28e6c65a28441305d83c8d04e155
natty4 <natty7kt@gmail.com>
2024-10-24 00:49:43 +0300

    Refactor static file handling in settings.py

--- Abooking/settings.py
+++ Abooking/settings.py
@@ -161,11 +161,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",  # Optional if you have custom static files directory
 # ]
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
